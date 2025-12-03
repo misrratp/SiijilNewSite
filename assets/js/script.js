b7a4fe565d2dce7ccf8ce716824da852e4655043
Misraim tut <72896962+misrratp@users.noreply.github.com>
2025-12-03 09:03:40 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
+/* ==============================================================
+   1. LÓGICA DEL MENÚ MÓVIL (PRIORIDAD ALTA)
+   ============================================================== */
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
 
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+// Función para abrir/cerrar menú
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < elem.length; i++) {
+    if (elem[i]) {
+      elem[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.body.classList.toggle("active"); // Bloquea el scroll
+      });
+    }
+  }
+}
+
+// Activar botones del menú
+if (navOpenBtn && navbar) {
+  navToggleEvent(navElemArr);
+}
 
 // Cerrar menú al dar clic en un enlace
 for (let i = 0; i < navLinks.length; i++) {
